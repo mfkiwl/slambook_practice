6c1b2cad9998d899fd44cb4c52965bd1a8cbac56
JeffCJ <jeffcj1125@gmail.com>
2023-12-03 13:44:33 -0800

    Practice cmake and build hello.cpp, libhellow.cpp, and libhello.hpp

---
+++ ch1_hello/hello.cpp
@@ -0,0 +1,9 @@
+#include "libhello.hpp"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << "hello" << endl;
+  print_hello();
+  return 0;
+}
---
+++ ch1_hello/libhello.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+void print_hello() { cout << "hello from libhello" << endl; }
